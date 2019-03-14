e3b03c13bcabe3bd6709963bea06d6098b1b8ffc
Pim Rheiter <pim.rheiter@student.hu.nl>
2019-03-14 17:27:22 +0100

    parse json

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/App.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/App.java
@@ -8,6 +8,9 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import nl.hu.bdsd.consumerCreator.constants.IKafkaConstants;
 import nl.hu.bdsd.consumerCreator.consumer.ConsumerCreator;
 
@@ -19,13 +22,27 @@ public class App {
 	static void runConsumer() throws IOException {
 		Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
 		consumer.subscribe(Arrays.asList(IKafkaConstants.TOPIC_NAME));
+		JsonParser parser = new JsonParser();
+		
 		while (true) {
-	         ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis((long) 100));
-	         for (ConsumerRecord<Long, String> record : records)
-	         
-	         // print the offset,key and value for the consumer records.
-	         System.out.printf("offset = %d, key = %s, value = %s\n", 
-	            record.offset(), record.key(), record.value());
-	      }
+			ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis((long) 100));
+			
+			for (ConsumerRecord<Long, String> record : records) {
+		    	JsonObject jsonObject = parser.parse(record.value()).getAsJsonObject();
+		    	
+		    	String title;
+		    	try {
+		    		title = String.format("title = \"%s\"",
+		    				jsonObject.get("_sources").getAsJsonObject().get("title").getAsString());
+		    	}
+		    	
+		    	catch (NullPointerException e){
+		    		title = "no title available";
+		    	}
+		    	
+				// print the key and title for the consumer records.
+				System.out.printf("key = %d, %s\n", record.key(), title);
+			}
+		}
 	}
 }
