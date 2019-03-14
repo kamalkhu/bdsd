f6ca3b845da9ec7f650b4651ef7dac4ee01ac5dc
kamalkhu <kamal.kamal@student.hu.nl>
2019-03-14 22:30:08 +0100

    Update ArtikelDAO.java

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/ArtikelDAO.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/ArtikelDAO.java
@@ -1,1 +1,15 @@
+package nl.hu.bdsd.consumerCreator.persistence;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
+@SuppressWarnings("deprecation")
+public class BaseDAO {
+
+	@SuppressWarnings("resource")
+	public DB Connection() {
+		MongoClient mongo = new MongoClient("localhost", 27017);
+		DB db = mongo.getDB("bdsd");
+		return db;
+	}
+}
