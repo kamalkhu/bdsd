e3b93e2fea186ec49cd27cb01118c525ae2d1cf1
kamalkhu <kamal.kamal@student.hu.nl>
2019-03-14 22:30:40 +0100

    Update BaseDAO.java

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
@@ -1,1 +1,17 @@
+package nl.hu.bdsd.consumerCreator.persistence;
 
+import java.util.Set;
+import com.mongodb.DB;
+
+public class ArtikelDAO extends BaseDAO{
+
+	public void getStuff() {
+		
+		DB db = super.Connection();
+		Set<String> tables = db.getCollectionNames();
+		
+		for(String coll : tables){
+			System.out.println(coll);
+		}
+	}
+}
