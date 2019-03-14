ca2769887b07597ba0c4fc66f99fcda58f47742b
kamalkhu <kamal.kamal@student.hu.nl>
2019-03-14 23:15:01 +0100

    Update BaseDAO.java

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
@@ -1,5 +1,6 @@
 package nl.hu.bdsd.consumerCreator.persistence;
 
+import java.util.Set;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
@@ -7,9 +8,14 @@ import com.mongodb.MongoClient;
 public class BaseDAO {
 
 	@SuppressWarnings("resource")
-	public DB connection() {
+	public void getStuff() {
 		MongoClient mongo = new MongoClient("localhost", 27017);
 		DB db = mongo.getDB("bdsd");
-		return db;
+
+		Set<String> tables = db.getCollectionNames();
+
+		for (String coll : tables) {
+			System.out.println(coll);
+		}
 	}
 }
