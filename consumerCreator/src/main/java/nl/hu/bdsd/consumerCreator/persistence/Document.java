b33576f7c495fd4c62b00b686afde0af6ede42b6
Pim Rheiter <pim.rheiter@student.hu.nl>
2019-03-15 00:05:11 +0100

    clean html and TFIDF in own package

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/Document.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/Document.java
@@ -8,6 +8,7 @@ import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
 public class Document {
+	public String _id;
 	public String title;
 	public String source;
 	public String text;
@@ -17,7 +18,8 @@ public class Document {
 	public HashMap<String, Double> idfScores;
 	public HashMap<String, Double> tfIdfScores;
 
-	public Document(String title, String source, String text, String location, String date) {
+	public Document(String _id, String title, String source, String text, String location, String date) {
+		this._id = _id;
 		this.title = title;
 		this.source = source;
 		this.text = text;
