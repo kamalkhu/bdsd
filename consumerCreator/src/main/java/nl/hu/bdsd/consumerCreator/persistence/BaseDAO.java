2d5665245d9ec9973e071eef375b0f43bcf2d771
kamalkhu <kamal.kamal@student.hu.nl>
2019-03-14 22:37:43 +0100

    Update BaseDAO.java

--- consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
+++ consumerCreator/src/main/java/nl/hu/bdsd/consumerCreator/persistence/BaseDAO.java
@@ -2,15 +2,18 @@ package nl.hu.bdsd.consumerCreator.persistence;
 
 import java.util.Set;
 import com.mongodb.DB;
+import com.mongodb.MongoClient;
 
-public class ArtikelDAO extends BaseDAO{
+@SuppressWarnings("deprecation")
+public class BaseDAO {
 
+	@SuppressWarnings("resource")
 	public void getStuff() {
-		
-		DB db = super.Connection();
+		MongoClient mongo = new MongoClient("localhost", 27017);
+		DB db = mongo.getDB("bdsd");
 		Set<String> tables = db.getCollectionNames();
-		
-		for(String coll : tables){
+
+		for (String coll : tables) {
 			System.out.println(coll);
 		}
 	}
